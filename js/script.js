c631442b554373f8389022e0dcc47cbc38e62275
NurzhanBoranbay <boranbaynurzhan@gmail.com>
2021-12-05 18:02:55 +0600

    create timer

--- js/script.js
+++ js/script.js
@@ -39,5 +39,58 @@ window.addEventListener('DOMContentLoaded', () => {
       }
    });
 
+  // Timer
 
+  const deadline = '2021-12-06';
+
+  function getTimeRemaining(endTime) {
+     const time = Date.parse(endTime) - Date.parse(new Date()),
+           days = Math.floor(time / (1000 * 60 * 60 * 24)),
+           hours = Math.floor(time / (1000 * 60 * 60) % 24),
+           minutes = Math.floor((time / 1000 / 60) % 60),
+           seconds = Math.floor((time / 1000) % 60);
+
+     return {
+        'total': time,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+     };
+  }
+
+  function getZero(num) {
+     if (num >= 0 && num < 10) {
+        return `0${num}`;
+     } else {
+        return num;
+     }
+  }
+
+  function setClock(selector, endTime) {
+     const time = document.querySelector(selector),
+           days = time.querySelector('#days'),
+           hours = time.querySelector('#hours'),
+           minutes = time.querySelector('#minutes'),
+           seconds = time.querySelector('#seconds'),
+           timeInterval = setInterval(updateClock, 1000);
+
+     updateClock();
+
+     function updateClock() {
+        const t = getTimeRemaining(endTime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+           clearInterval(timeInterval);
+        }
+
+     }
+  }
+
+  setClock('.timer', deadline);
 });
